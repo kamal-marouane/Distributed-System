9aecd454c7fc140b70792308a3a517f2fe1e275e
kamal-marouane <110986028+kamal-marouane@users.noreply.github.com>
2023-11-13 22:18:43 +0100

    Setting Client message from args

--- src/network/src/main/java/client/Client.java
+++ src/network/src/main/java/client/Client.java
@@ -5,9 +5,15 @@ import server.PingPongInterface;
 
 public class Client {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a message as a command-line argument.");
+            return;
+        }
+        String message = args[0];
+
         try {
             PingPongInterface stub = (PingPongInterface) Naming.lookup("rmi://localhost/PingPong");
-            String response = stub.ping("Hello, Server!");
+            String response = stub.ping(message);
             System.out.println("Response: " + response);
         } catch (Exception e) {
             System.out.println("Client exception: " + e);
